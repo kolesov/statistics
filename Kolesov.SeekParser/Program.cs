13982d84e743d9c841ab5cafae070c22534e569d
Sergey Kolesov <sergey.kolesov.gs@gmail.com>
2015-02-28 15:49:09 +1100

    Add project for parsing seek.com.au job

---
+++ Kolesov.SeekParser/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kolesov.SeekParser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string url = @"http://www.seek.com.au/job/28220831?pos=4&type=standard";
+            WebRequest request = WebRequest.Create(url);
+            request.Method = "GET";
+            WebResponse response = request.GetResponse();
+            Stream stream = response.GetResponseStream();
+            StreamReader reader = new StreamReader(stream);
+            string content = reader.ReadToEnd();
+            reader.Close();
+            response.Close();
+
+            Console.WriteLine(content);
+            Console.ReadKey();
+        }
+    }
+}
